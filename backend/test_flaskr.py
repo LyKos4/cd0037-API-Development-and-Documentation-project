dc100ee3c1ee5531c92797fc6972bf94dec3dc24
LyKos4 <85453998+LyKos4@users.noreply.github.com>
2023-12-24 20:41:26 +0200

    Update test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,8 +14,10 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        ##self.database_name = "trivia_test"
-        self.database_path = "postgres://postgres:postgres@127.0.0.1:5432/trivia_test"
+        self.database_name = "trivia_test"
+#        self.database_path = "postgres://postgres:postgres@127.0.0.1:5432/{}".format(self.database_name)
+        self.database_path = "postgresql://postgres:postgres@127.0.0.1:5432/trivia_test"
+      #  self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +26,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +35,7 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
     def test_get_questions(self):
         """Test getting all questions"""
         res = self.client().get('/questions')
@@ -42,24 +45,6 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['success'])
         self.assertTrue(len(data['questions']) > 0)
 
-    def test_get_categories(self):
-        """Test getting all categories"""
-        res = self.client().get('/categories')
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertTrue(len(data['categories']) > 0)
-
-    def test_get_questions_by_category_success(self):
-        """Test getting questions by a category that exists"""
-        existing_category_id = 1
-        res = self.client().get(f'/categories/{existing_category_id}/questions')
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertIsNotNone(data['questions'])
 
     def test_create_new_question_success(self):
         """Test creating a new question"""
@@ -76,14 +61,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['success'])
         self.assertIsNotNone(data['created'])
 
+
+
     def test_get_questions_by_category_fail(self):
         """Test getting questions by a category that doesn't exist"""
         non_existent_category_id = 999
         res = self.client().get(f'/categories/{non_existent_category_id}/questions')
-        data = json.loads(res.data)
 
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 404)
+        data = json.loads(res.data)
         self.assertFalse(data['success'])
+        self.assertEqual(data['error'], 404)
         self.assertEqual(data['message'], 'Not Found')
 
 # Make the tests conveniently executable
